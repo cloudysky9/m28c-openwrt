4158474d883a37686a905b4edce89f224ccbd7a8
cloudysky9 <64485673+cloudysky9@users.noreply.github.com>
2025-12-28 23:07:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,5 @@ sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/fil
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+# 添加软件包源
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
